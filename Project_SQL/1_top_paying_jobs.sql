0f31e06f6b980aa95c2c132e5662999e17a474d3
tobit bushenyula <mrbushenyula@gmail.com>
2024-04-17 16:51:04 +0300

    this is the first complete SQL query that gives us useful insights about the remote data anayst job opportunities, showcasing the most paid jobs in that field. and the company name.

--- Project_SQL/1_top_paying_jobs.sql
+++ Project_SQL/1_top_paying_jobs.sql
@@ -8,19 +8,36 @@ for my role? (Data Analyst)
 - Why? Highlight the top-paying opportunities for Data Analysts, offering insights.
 */
 
-SELECT 
-    job_id,
-    job_title,
-    job_location,
-    job_schedule_type,
-    salary_year_avg,
-    job_posted_date
 
-FROM 
-    job_postings_fact
-WHERE job_title_short='Data Analyst' 
-      and job_location='Anywhere' 
-      and salary_year_avg is not null
+WITH remote_data_anayst_top_jobs as 
+( 
+    SELECT 
+        job_id,
+        company_id,
+        job_title,
+        job_location,
+        job_schedule_type,
+        salary_year_avg,
+        job_posted_date
 
-order by salary_year_avg desc
-limit 10;
+    FROM 
+        job_postings_fact
+    WHERE job_title_short='Data Analyst' 
+        and job_location='Anywhere' 
+        and salary_year_avg is not null
+
+    order by salary_year_avg desc
+    limit 10
+)
+
+select cd.name, 
+       rdatj.job_title, 
+       rdatj.job_location, 
+       rdatj.job_schedule_type,
+       rdatj.salary_year_avg,
+       rdatj.job_posted_date
+from remote_data_anayst_top_jobs rdatj join company_dim cd
+on rdatj.company_id = cd.company_id;
+
+-- rdatj = remote_data_analyst_top_jobs (our CTE)
+
