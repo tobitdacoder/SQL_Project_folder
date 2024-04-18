67b2664f77f12f7b3370e955836f9bfb2b8eec3f
tobit bushenyula <mrbushenyula@gmail.com>
2024-04-18 08:07:18 +0300

    here I have updated the query to add the job posting table PRIMARY KEY to the list so that we can easily join it with other tables.

--- Project_SQL/2_top_skills_required_for_the_jobs_I_want.sql
+++ Project_SQL/2_top_skills_required_for_the_jobs_I_want.sql
@@ -19,34 +19,35 @@ create table top_data_analyst_remote_jobs_postings as
 
 (
 
-WITH remote_data_anayst_top_jobs as 
-( 
-        SELECT 
-            job_id,
-            company_id,
-            job_title,
-            job_location,
-            job_schedule_type,
-            salary_year_avg,
-            job_posted_date
+    WITH remote_data_anayst_top_jobs as 
+    ( 
+            SELECT 
+                job_id,
+                company_id,
+                job_title,
+                job_location,
+                job_schedule_type,
+                salary_year_avg,
+                job_posted_date
 
-        FROM 
-            job_postings_fact
-        WHERE job_title_short='Data Analyst' 
-            and job_location='Anywhere' 
-            and salary_year_avg is not null
+            FROM 
+                job_postings_fact
+            WHERE job_title_short='Data Analyst' 
+                and job_location='Anywhere' 
+                and salary_year_avg is not null
 
-        order by salary_year_avg desc
-        limit 10
-    )
-    select cd.name, 
-        rdatj.job_title, 
-        rdatj.job_location, 
-        rdatj.job_schedule_type,
-        rdatj.salary_year_avg,
-        rdatj.job_posted_date
-    from remote_data_anayst_top_jobs rdatj join company_dim cd
-    on rdatj.company_id = cd.company_id
+            order by salary_year_avg desc
+            limit 10
+        )
+        select cd.name,
+            rdatj.job_id, 
+            rdatj.job_title, 
+            rdatj.job_location, 
+            rdatj.job_schedule_type,
+            rdatj.salary_year_avg,
+            rdatj.job_posted_date
+        from remote_data_anayst_top_jobs rdatj join company_dim cd
+        on rdatj.company_id = cd.company_id
 
 );
 
